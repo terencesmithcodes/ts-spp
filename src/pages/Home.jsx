f31ff25092177b756802844e6470ca25219acecd
Terence Smith <terencesmith1@me.com>
2023-04-13 21:37:29 -0400

    initial push

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+// import terence from './assets/head-shot.jpg'
+
+function Home() {
+  return (
+    <>
+      <nav>Nav</nav>
+      <div>About</div>
+      {/* <img src={terence} alt="#" /> */}
+      <div>Skills</div>
+      <div>Projects</div>
+      <div>Contact</div>
+      <div>Footer</div>
+    </>
+  )
+}
+
+export default Home
